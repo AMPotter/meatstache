ca7dfb706da8cb0a8f59107fdbf17a15aea0a527
jbbrokaw <jbrokaw@gmail.com>
2014-08-26 12:59:57 -0700

    Makes price hiding work a little better

--- store-widgets.js
+++ store-widgets.js
@@ -4,11 +4,15 @@ $(function() {
   $('.product-box').children('p').hide();
 
   $('.store').on('mouseenter', '.product-box', function () {
-    $(this).children('p').show('slide', {direction: 'down'}, 250);
+    $(this).children('p').show('slide', {direction: 'down'}, 100);
   });
 
   $('.store').on('mouseleave', '.product-box', function () {
-    $(this).children('p').hide('slide', {direction: 'down'}, 250)
+    $(this).children('p').hide('slide', {direction: 'down'}, 100);
+  });
+
+  $('.store-row').on('mouseleave', function () {
+    $(this).children().children('p').hide('slide', {direction: 'down'}, 250);
   });
 
 });
